6f598195b13c1b8d1c926a012b18df47d4e03ff2
Justin Zelinsky <jpzelinsky@gmail.com>
2019-08-13 00:23:00 -0400

    Update info

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 fs.readFile('./info.json', 'utf-8', (err, data) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
 });
